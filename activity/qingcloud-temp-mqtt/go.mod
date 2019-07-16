3476d99a8577c49e381fe91ad152ddf9d3d85ffc
service clickhouse-server status <service clickhouse-server status>
2019-07-16 14:51:52 +0800

    add temp mqtt

---
+++ activity/qingcloud-temp-mqtt/go.mod
@@ -0,0 +1,11 @@
+module github.com/qingcloudhx/flow-plugin/activity/qingcloud_temp_mqtt
+
+go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/qingcloudhx/core v0.9.3-0.20190625065757-9a4c5da90847
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/net v0.0.0-20190514140710-3ec191127204 // indirect
+)
---
+++ activity/qingcloud-temp-mqtt/metadata.go
@@ -0,0 +1,46 @@
+package qingcloud_temp_mqtt
+
+import "github.com/qingcloudhx/core/data/coerce"
+
+type Settings struct {
+	Broker       string                 `md:"broker,required"` // The broker URL
+	Id           string                 `md:"id,required"`     // The id of client
+	Username     string                 `md:"username"`        // The user's name
+	Password     string                 `md:"password"`        // The user's password
+	Store        string                 `md:"store"`           // The store for message persistence
+	CleanSession bool                   `md:"cleanSession"`    // Clean session flag
+	Topic        string                 `md:"topic"`           // The topic to publish to
+	Qos          int                    `md:"qos"`             // The Quality of Service
+	SSLConfig    map[string]interface{} `md:"sslConfig"`       // SSL Configuration
+}
+
+type Input struct {
+	Message interface{} `md:"message"` // The message to send
+}
+
+type Output struct {
+	Data interface{} `md:"data"` // The data recieved
+}
+
+func (i *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"message": i.Message,
+	}
+}
+
+func (i *Input) FromMap(values map[string]interface{}) error {
+	i.Message, _ = coerce.ToObject(values["message"])
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"data": o.Data,
+	}
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+
+	o.Data = values["data"]
+	return nil
+}
