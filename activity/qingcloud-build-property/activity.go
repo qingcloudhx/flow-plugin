af88090cbfafbb865c75f0b3ef724bc6fd3518a5
hexing <hexing@yunify.com>
2019-08-02 17:30:15 +0800

    fix bug

--- activity/qingcloud-build-property/activity.go
+++ activity/qingcloud-build-property/activity.go
@@ -10,7 +10,7 @@ func init() {
 	_ = activity.Register(&Activity{}, New)
 }
 
-var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{Device: make(map[string]interface{})})
+var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
 
 func New(ctx activity.InitContext) (activity.Activity, error) {
 	settings := &Settings{}
